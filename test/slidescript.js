da5176c767e46f6e7b237ed4db16199c93883f3d
SchmidtJoaoPedro <joaoschmidtmagalhaes@gmail.com>
2025-04-18 14:06:26 -0300

    test

---
+++ test/slidescript.js
@@ -0,0 +1,13 @@
+const track = document.getElementById("carouselTrack");
+const slides = document.querySelectorAll(".slide");
+let currentIndex = 0;
+
+function moveSlide(direction) {
+  currentIndex += direction;
+
+  if (currentIndex < 0) currentIndex = slides.length - 1;
+  if (currentIndex >= slides.length) currentIndex = 0;
+
+  const offset = -currentIndex * 100;
+  track.style.transform = `translateX(${offset}%)`;
+}
